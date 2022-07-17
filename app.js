362f2d6c6fa9e006d720533632e481b0c51b3eb4
Pooja <poojabhakne9@gmail.com>
2022-07-17 14:15:54 +0530

    my Program

---
+++ app.js
@@ -0,0 +1,15 @@
+let count =0;
+var a=document.getElementById("count");
+
+function increment()
+{
+    count=count+1;
+    a.innerHTML=count;
+    
+}
+function decrement()
+{    
+   
+    count=count-1;
+    a.innerHTML=count;
+}
